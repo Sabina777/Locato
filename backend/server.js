60a4e6544bcfadb581eba80415ed2cc2d0f3d877
Sabina777 <sabina.prajapati777@gmail.com>
2025-04-02 09:41:58 +0100

    store controller logics added ofr crud operations, searching and pagination

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { sequelize, connectDB } = require("./config/db"); 
+const storeRoutes = require('./routes/storeRoute');
+
 // Initialize environment variables
 dotenv.config();
 
@@ -10,10 +12,12 @@ const app = express();
 // Connect to database
 connectDB();
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Locato Backend is running');
-});
+// Middleware to parse JSON request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+  
+app.use('/api', storeRoutes);
+
 
 // Start the server
 const port = process.env.PORT || 5000;
